bd75bd735961dd50eb90f8cc07c6472cfdb59156
Daniel <syqrefind@gmail.com>
2021-05-04 15:25:06 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * @author daniel shen
+ * 
+ * This application retrieves and prints information about the top five ice cream shops in Alpharetta, Georgia via Yelp Fusion API.
+ * https://www.yelp.com/fusion
+ * 
+ * Info:
+ * o business name 
+ * o business address (street, city) 
+ * o excerpt from a review of that business 
+ * o name of the person that wrote the review 
+ * o business information should be output in the order received from the API response 
+ */
+
+
+module.exports = (() => {
+  console.log('hello')
+})();
+
+
+
+
+
+
